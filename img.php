f323614a370f0480ccbe5615f70f2868fd983edc
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2022-02-03 22:16:47 +0100

    Delete img.php

--- img.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (empty($lol)) {
-  $a = "Lol";
-}
-header("Content-Type: image/jpg");
-header("Content-Length: " . filesize("protected/disk/diskNumber139/Fox.jpg"));
-readfile("protected/disk/diskNumber139/Fox.jpg");
