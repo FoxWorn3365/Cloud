b35e5c68b57b8826a5ed972bcda20976cd584a34
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-07-02 19:37:06 +0200

    Aggiunta la barra di caricamento
    Finalmente ho aggiunto la barra di caricamento per i file in modo che si veda quanto manca :D

--- protected/components/uploadAFile.php
+++ protected/components/uploadAFile.php
@@ -6,11 +6,53 @@ $bb = str_replace("%28", "(", $bb);
 $bb = str_replace("%29", ")", $bb);
 
 ?>
+<div id='formUpload'>
+ <form id="upload_form" enctype="multipart/form-data" method="post">
+  <input type="file" name="file1" id="file1" onchange="uploadFile()"><br>
+  <progress id="progressBar" value="0" max="100" style="width:300px;"></progress>
+  <h3 id="status"></h3>
+  <p id="loaded_n_total"></p>
+ </form>
+</div>
 
-<form enctype="multipart/form-data" action="/up" method="POST">
-  <input type="hidden" name="user" value="<?= $pp[0]; ?>">
-  <input type="hidden" name="dir" value="<?= $bb; ?>">
-  <input type="hidden" name="MAX_FILE_SIZE" value="<?= $freebytes; ?>">
-  Invia questo file: <input name="userfile" type="file"></br>
-  <input type="submit" value="Invia File">
-</form>
+<script>
+function _(el) {
+  return document.getElementById(el);
+}
+
+function uploadFile() {
+  var file = _("file1").files[0];
+  // alert(file.name+" | "+file.size+" | "+file.type);
+  var formdata = new FormData();
+  formdata.append("userfile", file);
+  formdata.append("dir", "<?= $bb; ?>");
+  var ajax = new XMLHttpRequest();
+  ajax.upload.addEventListener("progress", progressHandler, false);
+  ajax.addEventListener("load", completeHandler, false);
+  ajax.addEventListener("error", errorHandler, false);
+  ajax.addEventListener("abort", abortHandler, false);
+  ajax.open("POST", "/up"); // http://www.developphp.com/video/JavaScript/File-Upload-Progress-Bar-Meter-Tutorial-Ajax-PHP
+  //use file_upload_parser.php from above url
+  ajax.send(formdata);
+}
+
+function progressHandler(event) {
+  _("loaded_n_total").innerHTML = "Caricati " + (event.loaded/1000000) + "MB su " + (event.total/1000000) + "MB";
+  var percent = (event.loaded / event.total) * 100;
+  _("progressBar").value = Math.round(percent);
+  _("status").innerHTML = Math.round(percent) + "% uploaded... please wait";
+}
+
+function completeHandler(event) {
+  _("status").innerHTML = event.target.responseText;
+  _("progressBar").value = 0; //wil clear progress bar after successful upload
+}
+
+function errorHandler(event) {
+  _("status").innerHTML = "Upload Failed";
+}
+
+function abortHandler(event) {
+  _("status").innerHTML = "Upload Aborted";
+}
+</script>
