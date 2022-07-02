ff1045bd13d5bf58b736e765a02764ccb48cf487
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-07-02 18:22:25 +0200

    Aggiornato il file per visualizzare i file
    Implementata in modo definitivo la visualizzazzione con la tabella, più sicura ed ordinata.
    inoltre ora con quella piccola parte in JS si possono evidenziare i file

--- protected/components/build_files.php
+++ protected/components/build_files.php
@@ -13,14 +13,22 @@
  $bb = str_replace("%29", ")", $bb);
 
  require_once("protected/components/files_header.php");
-?>
- <div style="position: absolute; left: 50px; text-align: left; width: 50%">
-<?php
  $count = 0;
  foreach(glob("protected/disk/$user->dir/$bb") as $file) {
    if (is_dir($file)) {
      $f = str_replace(str_replace("%20", " ", $replace), "", $file);
-     echo "<i class='fa-solid fa-folder'></i> <a href='/u/$pp[0]/files/$h$f/'><b>$f/</b></a><a href='/u/$pp[0]/deleteDirectory/$h$f' style='position: absolute; text-align: right; right: -80%'><i class='fa fa-trash' aria-hidden='true'></i></a><a style='position: absolute; text-align: right; right: -90%' href='/u/$pp[0]/rename/$h$f'><i class='fa-solid fa-pen-to-square'></i></a><br>";
+?>
+  <tr>
+   <td style='text-align: left'><a onclick="evidenziaFile('<?= $f . $h; ?>')"><i class='fa-solid fa-folder'></i></a> <a href='/u/<?= $pp[0]; ?>/files/<?= $h.$f; ?>/'><b><?= $f; ?>/</b></a></td>
+   <td></td>
+   <td></td>
+   <td><a href='/u/<?= $pp[0]; ?>/deleteDirectory/<?= $h.$f; ?>'><i class='fa fa-trash' aria-hidden='true'></i></a></td>
+   <td></td>
+   <td><a href='/u/<?= $pp[0]; ?>/rename/<?= $h.$f; ?>'><i class='fa-solid fa-pen-to-square'></i></a></td>
+  </tr>
+<?php
+
+     // echo "<a onclick=\"evidenziaFile($count)\"><i class='fa-solid fa-folder'></i></a> <a href='/u/$pp[0]/files/$h$f/'><b>$f/</b></a><a href='/u/$pp[0]/deleteDirectory/$h$f' style='position: absolute; text-align: right; right: -80%'><i class='fa fa-trash' aria-hidden='true'></i></a><a style='position: absolute; text-align: right; right: -90%' href='/u/$pp[0]/rename/$h$f'><i class='fa-solid fa-pen-to-square'></i></a><br>";
    } else {
      $f = str_replace(str_replace("%20", " ", $replace), "", $file);
 
@@ -41,10 +49,51 @@
      } else {
        $icona = '<i class="fa-solid fa-file"></i>';
      }
-     echo "$icona <u><a href='/u/$pp[0]/fileopen/$h$f'>$f</a> <span style='position: absolute; text-align: right; right: -50%'>". filesize($file)/1000 . "</span><a href='/download?url=$h$f' style='position: absolute; text-align: right; right: -75%'><i class='fa fa-cloud-download' aria-hidden='true'></i></a> <a style='position: absolute; text-align: right; right: -80%' href='/u/$pp[0]/deleteFile/$h$f'><i class='fa fa-trash' aria-hidden='true'></i></a><a style='position: absolute; text-align: right; right: -85%' href='/u/$pp[0]/share/$h$f'><i class='fa-solid fa-share-from-square'></i></i></a><a style='position: absolute; text-align: right; right: -90%' href='/u/$pp[0]/rename/$h$f'><i class='fa-solid fa-pen-to-square'></i></a></u><br>";
-     $count++;
+?>
+  <tr id='<?= $f . $h; ?>'>
+   <td style='text-align: left'><a onclick="evidenziaFile('<?= $h . $f; ?>')"><?= $icona; ?></a> <u><a href='/u/<?= $pp[0]; ?>/fileopen/<?= $h.$f; ?>'><?= $f; ?></a></u></td>
+   <td><?= round(filesize($file)/1000, 2); ?> KB</td>
+   <td><a href='/download?url=<?= $h.$f; ?>'><i class='fa fa-cloud-download' aria-hidden='true'></i></a></td>
+   <td><a href='/u/<?= $pp[0]; ?>/deleteFile/<?= $h.$f; ?>'<i class='fa fa-trash' aria-hidden='true'></i></a></td>
+   <td><a href='/u/<?= $pp[0]; ?>/share/<?= $h.$f; ?>'><i class='fa-solid fa-share-from-square'></i></a></td>
+   <td><a href='/u/<?= $pp[0]; ?>/rename/<?= $h.$f; ?>'><i class='fa-solid fa-pen-to-square'></i></td>
+  </tr>
+<?php
+     // echo "<a onclick=\"evidenziaFile($count)\">$icona</a> <u><a href='/u/$pp[0]/fileopen/$h$f'>$f</a> <span style='position: absolute; text-align: right; right: -50%'>". filesize($file)/1000 . "</span><a href='/download?url=$h$f' style='position: absolute; text-align: right; right: -75%'><i class='fa fa-cloud-download' aria-hidden='true'></i></a> <a style='position: absolute; text-align: right; right: -80%' href='/u/$pp[0]/deleteFile/$h$f'><i class='fa fa-trash' aria-hidden='true'></i></a><a style='position: absolute; text-align: right; right: -85%' href='/u/$pp[0]/share/$h$f'><i class='fa-solid fa-share-from-square'></i></i></a><a style='position: absolute; text-align: right; right: -90%' href='/u/$pp[0]/rename/$h$f'><i class='fa-solid fa-pen-to-square'></i></a></u><br>";
    }
+   $count++;
  }
  ?>
- <br><br><br><br><br><br><br>
- </div>
+  <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+ </table>
+ <br><br><br><br><br><br><br><br><br><br>
+ <script src='/http.js'></script>
+ <script>
+ function evidenziaFile(number) {
+    var res = JSON.parse(http_request('/evidenziaFile?file=' + number));
+    alert(res.message);
+    location.reload(); 
+ }
+
+ document.getElementById('body').addEventListener('load', loadPage());
+
+ function loadPage() {
+   loadTableWidth();
+   loadFileEvidenziati();
+ }
+
+ function loadFileEvidenziati() {
+   var res = JSON.parse(http_request('/getEvidenziati'));
+   if (res.presence == true) {
+     for (let a = 0; a < 250; a++) {
+       if (res.evidenziati[a] != "" && document.getElementById(res.evidenziati[a]) != null) {
+         document.getElementById(res.evidenziati[a]).style.backgroundColor = "yellow";
+       }
+     }
+   }
+ }
+
+ function loadTableWidth() {
+   document.getElementById('mytable').style.width = (screen.width - 50) + "px";
+ } 
+ </script>
