90e5e26bbffc789fbb0bb1f81e557b864ee4923e
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-07-09 00:35:07 +0200

    Arriva la dark mode
    Finalmente ho implementato al volo la dark mode con tanto di cookie per coloro che lo vogliono usare alle 3 di notte senza essere abbagliati dalla luce divina.
    Ovviamente tutto in Js :(

--- protected/components/header.php
+++ protected/components/header.php
@@ -14,7 +14,54 @@
  </head>
  <body style="text-align: center; background: white" id='body'>
   <div id='footer' class='w3-container w3-bottom w3-white'  style='padding: 5px'>
-   <span>&copy; 2021 - 2022 <a href='https://foxcloud.fcosma.it/'>FoxCloud</a> by Federico Cosma (FoxWorn3365) | Rilasciato sotto <a href='https://fcosma.it/FBM/view?licenza=71hued189'><img src='https://fcosma.it/FBM/fbm_white.png' height="20" width="50"></a></span>
+   <span>&copy; 2021 - 2022 <a href='https://foxcloud.fcosma.it/'>FoxCloud</a> by Federico Cosma (FoxWorn3365) | Rilasciato sotto <a href='https://fcosma.it/FBM/view?licenza=71hued189'><img src='https://fcosma.it/FBM/fbm_white.png' height="20" width="50"></a><a class='w3-right w3-button' onclick='changeTon()'><i id='ton' class="fa-solid fa-sun"></i></a></span>
   </div>
+  <script>
+  function isInDarkMode() {
+    if (document.cookie.split(';').some((item) => item.includes('mode=dark'))) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
+  // caricamento
+  document.getElementById('body').addEventListener('load', checkView());
+  
+  function checkView() {
+    if (isInDarkMode()) {
+      document.getElementById('ton').class = "fa-solid fa-sun"
+      changeTon();
+    } else {
+      document.getElementById('ton').class = "fa-solid fa-moon"
+      changeTon();
+    }
+  }
+
+  function setViewCookie(mode) {
+    if (mode == "dark") {
+      document.cookie = "mode=dark; Secure";
+    } else {
+      document.cookie = "mode=light; Secure";
+    }
+  }
+      
+  function changeTon() {
+    if (document.getElementById('ton').class == "fa-solid fa-sun") {
+      // Mettiamo in modalità notte
+      document.getElementById('ton').class = "fa-solid fa-moon";
+      document.getElementById('body').style.color = "white";
+      document.getElementById('body').style.backgroundColor = "black";
+      document.getElementById('footer').classList = "w3-container w3-bottom w3-black w3-text-white";
+      setViewCookie('dark');
+    } else {
+      document.getElementById('ton').class = "fa-solid fa-sun";
+      document.getElementById('body').style.color = "black";
+      document.getElementById('body').style.backgroundColor = "white";
+      document.getElementById('footer').classList = "w3-container w3-bottom w3-white w3-text-black";
+
+      setViewCookie('light');
+    }
+  }
+  </script>
  <?php require_once("protected/components/menu.php"); ?>
