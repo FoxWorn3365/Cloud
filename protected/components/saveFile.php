d9f97cfec603f8e4b8e6ef14afd8d935857a4873
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-07-12 23:02:49 +0200

    Aggiunto qualche placeholder, come nel file .IDEA

--- protected/components/saveFile.php
+++ protected/components/saveFile.php
@@ -1,8 +1,16 @@
 <?php
 $contents = filter_var($_POST["file"], FILTER_SANITIZE_STRING);
+$bb = urlEncode($bb);
 
 if (empty($content)) {
   die("Content");
+}
+
+if (end(explode('.', $bb)) == "fox") {
+  // Ci divertiamo con i placeholders ed il markdown
+  $contents = str_replace("%data%", date("d/m/Y - H:i"), $contents);
+  $contents = str_replace("%img%", "<img src='", $contents);
+  $contents = str_replace("%/img%", "'>", $contents);
 }
 
 $h = fopen('protected/disk/' . $user->dir . '/' . $bb, "w+");
