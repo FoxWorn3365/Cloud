072013d19a25001cfca19f36273ca478745873b5
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-07-15 22:45:52 +0200

    Aggiornato il file per la rimozione di utenti
    Ora per evitare l'eliminazione diretta di utenti per errore il Cloud chiede conferma

--- protected/components/delete_user.php
+++ protected/components/delete_user.php
@@ -1,22 +1,37 @@
 <?php
-function rrmdir($dir) { 
-  if (is_dir($dir)) { 
-    $objects = scandir($dir);
-    foreach ($objects as $object) { 
-      if ($object != "." && $object != "..") { 
-        if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object))
-          rrmdir($dir. DIRECTORY_SEPARATOR .$object);
-        else
-          unlink($dir. DIRECTORY_SEPARATOR .$object); 
-      } 
+require_once("protected/components/header.php");
+?>
+  <h1>Eliminazione di un Utente</h1>
+  <br><br>
+  <b>Sei veramente sicuro di voler eliminare l'utente <?= $us; ?>?</b>
+  Ricordiamo che eliminando l'utente si andranno anche ad eliminare tutti i suoi file in modo IRREVERSIBILE!
+  <br><br>
+  <button class='w3-button w3-text-white w3-red' onclick='deleteUser()'>Procedi</button>&nbsp;&nbsp;&nbsp;<button class='w3-button w3-text-white w3-green' onclick='returnBack()'>Annulla</button><br><br>
+  <br><br><br><br>
+  <div id='result' class='w3-gray w3-text-white w3-display-middle' style='display: none'>
+    <h2 id='tit'>Codice di stato: <span id='statCode'></span></h2>
+    <span id='message'></span><br><br>
+    <button onclick='returnBack()' class='w3-button w3-orange w3-text-white'>Torna alla Dashboard</button><br><br>
+  </div>
+  <script src='/http.js'></script>
+  <script>
+  function deleteUser() {
+    var user = "<?= $us; ?>";
+    var res = JSON.parse(http_request('/admin.php?action=delete&user=' + user));
+    if (res.status == 200) {
+      document.getElementById('result').style.display = "block";
+      document.getElementById('statCode').innerHTML = res.status;
+      document.getElementById('message').innerHTML = res.message;
+      document.getElementById('tit').classList.add('w3-green');
+    } else {
+      document.getElementById('result').style.display = "block";
+      document.getElementById('statCode').innerHTML = res.status;
+      document.getElementById('message').innerHTML = res.message;
+      document.getElementById('tit').classList.add('w3-red');
     }
-    rmdir($dir); 
-  } 
-}
+  }
 
-$config = json_decode(file_get_contents('protected/users/' . $us . '/userinfo.conf'));
-if (!empty($config->dir)) {
-  rrmdir('protected/disk/' . $config->dir . '/');
-}
-
-header("Location: /admin/mep");
+  function returnBack() {
+    window.location.href = '/admin/mep';
+  }
+  </script>
