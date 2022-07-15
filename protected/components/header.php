8e7955c494fa05dbd8086e0af50db918c3155293
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-07-15 23:21:25 +0200

    Patch riguardante la dark mode
    Ora finalmente il sole cambia quando viene cambiata la modalità dove entra in gioco una meravogliosa luna.
    Sento comunque una forte puzza di easter-egg, io personalmente controllerei nello <script>//..//</script> poi non so voi

--- protected/components/header.php
+++ protected/components/header.php
@@ -13,6 +13,11 @@
   <link rel="stylesheet" href="https://cdn.rgbcraft.com/static/fontawesome/css/all.min.css">
  </head>
  <body style="text-align: center; background: white" id='body'>
+  <div id='debug' style='display: none'>
+   <span id='temp_1'></span>
+   <span id='temp_2'></span>
+   <span id='debug_1'></span>
+  </div>
   <div id='footer' class='w3-container w3-bottom w3-white'  style='padding: 5px'>
    <span>&copy; 2021 - 2022 <a href='https://foxcloud.fcosma.it/'>FoxCloud</a> by Federico Cosma (FoxWorn3365) | Rilasciato sotto <a href='https://fcosma.it/FBM/view?licenza=71hued189'><img src='https://fcosma.it/FBM/fbm_white.png' height="20" width="50"></a><a class='w3-right w3-button' onclick='changeTon()'><i id='ton' class="fa-solid fa-sun"></i></a></span>
   </div>
@@ -30,10 +35,10 @@
   
   function checkView() {
     if (isInDarkMode()) {
-      document.getElementById('ton').class = "fa-solid fa-sun"
+      document.getElementById('ton').classList = "fa-solid fa-sun";
       changeTon();
     } else {
-      document.getElementById('ton').class = "fa-solid fa-moon"
+      document.getElementById('ton').classList = "fa-solid fa-moon";
       changeTon();
     }
   }
@@ -47,19 +52,24 @@
   }
       
   function changeTon() {
-    if (document.getElementById('ton').class == "fa-solid fa-sun") {
+    document.getElementById('temp_1').innerHTML = Number(document.getElementById('temp_1').innerHTML) + 1;
+ 
+    if (Number(document.getElementById('temp_1').innerHTML) == 5) {
+      window.location.href = "/admin/easter-egg/bazinga";
+    }
+
+    if (document.getElementById('ton').classList == "fa-solid fa-sun") {
       // Mettiamo in modalità notte
-      document.getElementById('ton').class = "fa-solid fa-moon";
+      document.getElementById('ton').classList = "fa-solid fa-moon";
       document.getElementById('body').style.color = "white";
       document.getElementById('body').style.backgroundColor = "black";
       document.getElementById('footer').classList = "w3-container w3-bottom w3-black w3-text-white";
       setViewCookie('dark');
     } else {
-      document.getElementById('ton').class = "fa-solid fa-sun";
+      document.getElementById('ton').classList = "fa-solid fa-sun";
       document.getElementById('body').style.color = "black";
       document.getElementById('body').style.backgroundColor = "white";
       document.getElementById('footer').classList = "w3-container w3-bottom w3-white w3-text-black";
-
       setViewCookie('light');
     }
   }
