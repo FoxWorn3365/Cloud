e0e5f9824dc52743ac8f6bce6b0ef11effbb0bec
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-07-15 22:42:57 +0200

    Aggiunte per la sicurezza
    Ora è possibile sospendere l'accesso a tutti gli utenti

---
+++ protected/components/banned.php
@@ -0,0 +1,6 @@
+<?php
+require_once("protected/components/header.php");
+?>
+  <h1 style='color: red'><b>Sei stato sospeso!</b></h1>
+  <span><b>Al momento non puoi accedere al tuo account nel Cloud</b> per scelta dell'amministratore del Cloud.<br>
+  Al momento i tuoi file sono ancora <u>presenti ed al sicuro</u> nella tua cartella.</span>
