f7515d848d8869d4ebc2765d71e464db90e4312f
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-07-24 12:08:56 +0200

    Aggiornata la DarkMode
    Finalmente ora i file evidenziati hanno la scritta nera e non bianca

--- protected/components/build_files.php
+++ protected/components/build_files.php
@@ -86,6 +86,9 @@
      for (let a = 0; a < 250; a++) {
        if (res.evidenziati[a] != "" && document.getElementById(res.evidenziati[a]) != null) {
          document.getElementById(res.evidenziati[a]).style.backgroundColor = "yellow";
+         if (isInDarkMode()) {
+           document.getElementById(res.evidenziati[a]).style.color = "white";
+         }
        }
      }
    }
