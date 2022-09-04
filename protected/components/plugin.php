e21e8ba35678ec87aa491a2fd7ad7384a3e52148
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-09-04 11:51:03 +0200

    Implementato il sistema di caricamento ad eventi
    Tramite questo sistema i plugin non verranno più limitati solo al loadAfter oppure al loadBefore ma supporteranno anche ambedue gli eventi insieme.
    Verrà anche ammodernata la configurazione

--- protected/components/plugin.php
+++ protected/components/plugin.php
@@ -4,7 +4,7 @@
 // +--------------------------+
 // | (C) 2021 - 2022 .FoxOrg  |
 // | https://foxorg.fcosma.it |
-// | Tutti i diritti ris.     |
+// | Licenza GPL 3.0          |
 // +--------------------------+
 
 namespace FoxCloud;
@@ -12,19 +12,41 @@ namespace FoxCloud;
 class Plugins {
 
     protected string $folder;
-    protected array $loadAfter = array();
+    protected array $pluginListAfter = array();
+    protected array $pluginListBefore = array();
+    protected $savedBefore = array();
+    protected $savedAfter = array();
     protected string $confDir;
 
     public function setPluginsFolder($dir) {
       $this->folder = $dir;
     }
 
+    public function addAfter($value) {
+      array_push($this->pluginListAfter, $value);
+    }
+
+    public function addBefore($value) {
+      array_push($this->pluginListBefore, $value);
+    }
+
     protected function loadPlugin($name) {
       if ($this->pluginExists($name)) {
         require_once('protected/' . $this->folder . '/' . $name);
+        $this->log("", "[PluginManager] Plugin $name inizializzato con successo!");
       } else {
-        error_log("[FoxCloud] Il plugin richiesto non esiste! - Thrown in FoxCloud - Plugin: $name", 0);
+        $this->log("error", "[PluginManager] Il plugin richiesto non esiste! - Thrown in FoxCloud - Plugin: $name", 0);
       }
+    }
+
+    protected function log($type = '', $content) {
+      if ($type == "error") {
+        $pre = "[#ERROR]";
+      } else {
+        $pre = "[#INFO]";
+      }
+
+      file_put_contents('protected/sys/latest.log', file_get_contents('protected/sys/latest.log') . "\n[" . date("d/m/Y - H:i:s") . "]:[FoxCloud]:$pre $content");
     }
 
     protected function getFile($file) {
@@ -35,7 +57,7 @@ class Plugins {
       if ($this->pluginExists($name)) {
         return json_decode($this->getFile('phar://protected/' . $this->folder . '/' . $name . '/config.json'));
       } else {
-        error_log("[FoxCloud] Il plugin richiesto non esiste! - Thrown in FoxCloud - Plugin: $name", 0);
+        $this->log('error', "[PluginManager] Il plugin richiesto non esiste! - Thrown in FoxCloud - Plugin: $name", 0);
       }
     }
 
@@ -44,7 +66,7 @@ class Plugins {
       if ($pluginConf->type == "phar_plugin" && in_array($this->getFile('version.txt'), $pluginConf->compatibility->cloudVersions)) {
         return true;
       } else {
-        error_log("[FoxCloud] Il plugin richiesto non è caricato correttamente! - Thrown in FoxCloud - Plugin: $name - String: phar://protected/" . $this->folder . "/" . $name . "/config.json", 0);
+        $this->log('error', "[PluginManager] Il plugin richiesto non è caricato correttamente! - Thrown in FoxCloud - Plugin: $name - String: phar://protected/" . $this->folder . "/" . $name . "/config.json - Il plugin non presenta una corretta configurazione in config.json", 0);
         return false;
       }
     }
@@ -67,33 +89,11 @@ class Plugins {
       return $list;
     }
 
-    function loadAfter() {
-      if (!empty($this->loadAfter[0])) {
-        foreach ($this->loadAfter as $plugin) {
-          require 'protected/' . $this->folder . '/' . $plugin;
-        }
-      }
-
-      return true;
-    }
-
     protected function loadAllPlugins($url) {
       foreach ($this->pluginList() as $plugin) {
         $pluginConf = $this->getPluginConfig($plugin);
-        if ($pluginConf->loadEverywhere === true) {
-          if ($pluginConf->load == "before") {
+        if ($pluginConf->enabled === true) {
             $this->loadPlugin($plugin);
-          } else {
-            array_push($this->loadAfter, $plugin);
-          }
-        } else { 
-          if (in_array($url, $pluginConf->URLs) && $pluginConf->enable === true) {
-            if ($pluginConf->load == "before") {
-              $this->loadPlugin($plugin);
-            } else {
-              array_push($this->loadAfter, $plugin);
-            }
-          }
         }
       }
       return true;
@@ -107,42 +107,76 @@ class Plugins {
       return json_decode($this->getFile('protected/' . $this->confDir));
     }
 
-    public function loadPluginsFromConfig($url) {
+    public function loadPluginsFromConfig() {
       $config = $this->getGlobalPluginConfig();
       foreach ($config->load as $plugin) {
         if (!$this->pluginExists($plugin)) {
-          error_log("[FoxCloud] ERRORE: Nel file iniziale plugin_config.json un plugin ($plugin) non esiste!");
+          $this->log('error', "[PluginManager] ERRORE: Nel file iniziale plugin_config.json un plugin ($plugin) non esiste!");
           return false;
         }
         $pluginConf = $this->getPluginConfig($plugin);
-        if ($pluginConf->loadEverywhere == true) {
-          if ($pluginConf->load == "before") {
-            $this->loadPlugin($plugin);
-          } else {
-            array_push($this->loadAfter, $plugin);
-          }
-        } else { 
-          if (in_array($url, $pluginConf->URLs) && $pluginConf->enable === true) {
-            if ($pluginConf->load == "before") {
-              $this->loadPlugin($plugin);
-            } else {
-              array_push($this->loadAfter, $plugin);
-            }
-          }
-        }
+        if ($pluginConf->enabled === true) {
+          $this->loadPlugin($plugin);
+        } 
       }
       return true;
     }
 
-    public function addEventListener($url) {
+    public function test() {
+      die("OKKe");
+    }
+
+    public function load() {
       if ($this->getGlobalPluginConfig()->isEnabled == true && $this->getGlobalPluginConfig()->restricted == true) {
         $this->loadPluginsFromConfig($url);
       } elseif ($this->getGlobalPluginConfig()->isEnabled == true && $this->getGlobalPluginConfig()->restricted == false) {
         $this->loadAllPlugins($url);
       } else {
-        error_log("[FoxCloud] Plugins non abilitati!", 0);
+        $this->log('error', "[PluginManager] Plugins non abilitati!", 0);
         // Non carichiamo plugins
       }
       return true;
     }
+
+    public function addEvent($event, $type, $do, $params = array()) {
+      file_put_contents('protected/sys/temp.txt', $do);
+      if ($event == "pageLoad") {
+        if ($type == "before") {
+          array_push($GLOBALS['pluginLoadBefore'], $do);
+          $this->log("", "[PluginManager] Plugin aggiunto all'evento loadBefore!");
+        } elseif ($type == "after") {
+          array_push($GLOBALS['pluginLoadAfter'], $do);
+          $this->log("", "[PluginManager] Plugin aggiunto all'evento loadAfter!");
+        } else {
+          $this->log('error', "[PluginManager] NoEventTypeOn: PageLoad!", 0);
+        }
+      } elseif ($event == "request") {
+        if (is_array($params) && in_array($GLOBALS['url'], $params)) {
+          if ($type == "before") {
+            array_push($GLOBALS['pluginLoadBefore'], $do);
+            $this->log("", "[PluginManager] Plugin aggiunto all'evento loadBefore!");
+          } elseif ($type == "after") {
+            array_push($GLOBALS['pluginLoadAfter'], $do);
+            $this->log("", "[PluginManager] Plugin aggiunto all'evento loadAfter!");
+          } else {
+            $this->log('error', "[PluginManager] NoEventTypeOn: PageLoad!", 0);
+          }
+        }
+      }
+    }
+
+    public function execute($type = 'null') {
+      if ($type == "after") {
+        foreach ($GLOBALS['pluginLoadAfter'] as $action) {
+          $action();
+          $this->log("", "[PluginManager] Caricato i plugin dagli eventi pageLoad // AFTER");
+        }
+      } else {
+        foreach ($GLOBALS['pluginLoadBefore'] as $action) {
+          $action();
+          $this->log("", "[PluginManager] Caricato i plugin dagli eventi pageLoad // BEFORE");
+        }
+      }
+    }
 }
+
