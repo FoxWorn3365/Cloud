8d7c03a977500192f05998243297634ecb097321
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:16:27 +0100

    Update wiew_header.php

--- protected/components/wiew_header.php
+++ protected/components/wiew_header.php
@@ -1,4 +1,20 @@
 <?php
+// +-----------------------------------+
+// |             FoxCloud              |
+// +-----------------------------------+
+// | Questo file fa parte del progetto |
+// | di Cloud Open Source "FoxCloud",  |
+// | realizzato da FoxWorn.            |
+// +-----------------------------------+
+// | Web: https://foxcloud.fcosma.it   |
+// | GH: github.com/FoxWorn3365/Cloud  |
+// | License: GNU GPL 3.0              |
+// +-----------------------------------+
+// | You can write me an email at:     |
+// | foxworn3365@gmail.com, also for   |
+// | talk!                             |
+// +-----------------------------------+
+
 $textAbout = explode('/', $bb);
 for ($a = 0; $a < count($textAbout)-1; $a++) {
   $list = $list . $textAbout[$a] . '/';
