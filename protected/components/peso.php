7fa2df01175fcf578d3544e4dc2a7844e9346585
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:12:24 +0100

    Update peso.php

--- protected/components/peso.php
+++ protected/components/peso.php
@@ -1,5 +1,21 @@
 <?php
-// mrw.it
+// +-----------------------------------+
+// |             FoxCloud              |
+// +-----------------------------------+
+// | Questo file fa parte del progetto |
+// | di Cloud Open Source "FoxCloud",  |
+// | realizzato da FoxWorn.            |
+// +-----------------------------------+
+// | Web: https://foxcloud.fcosma.it   |
+// | GH: github.com/FoxWorn3365/Cloud  |
+// | License: GNU GPL 3.0              |
+// +-----------------------------------+
+// | You can write me an email at:     |
+// | foxworn3365@gmail.com, also for   |
+// | talk!                             |
+// +-----------------------------------+
+
+// Fonte: https://mrw.it
 function dirsize($dir)
 {
   // apro la cartella
