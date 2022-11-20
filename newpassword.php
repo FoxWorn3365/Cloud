0b32e0b192fc650f9f3f2dbf8fc1a03d93c66e64
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:21:57 +0100

    Update newpassword.php

--- newpassword.php
+++ newpassword.php
@@ -1,4 +1,20 @@
 <?php
+// +-----------------------------------+
+// |             FoxCloud              |
+// +-----------------------------------+
+// | Questo file fa parte del progetto |
+// | di Cloud Open Source "FoxCloud",  |
+// | realizzato da FoxWorn.            |
+// +-----------------------------------+
+// | Web: https://foxcloud.fcosma.it   |
+// | GH: github.com/FoxWorn3365/Cloud  |
+// | License: GNU GPL 3.0              |
+// +-----------------------------------+
+// | You can write me an email at:     |
+// | foxworn3365@gmail.com, also for   |
+// | talk!                             |
+// +-----------------------------------+
+
 session_start();
 if (file_exists("protected/users/" . $_SESSION['user']. "/userpass.conf")) {
    die("Non hai i permessi sufficenti!");
