dd4586701079ba896b0dada2dfc76690ff67993f
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 21:49:18 +0100

    Restyling completo della visualizzazione dei file

--- protected/components/build_files.php
+++ protected/components/build_files.php
@@ -1,4 +1,9 @@
  <br>
+ <style>
+ a {
+  text-decoration: none;
+ }
+ </style>
  <?php
  if (empty($bb)) {
   $replace = 'protected/disk/' . $user->dir. '/';
@@ -16,14 +21,10 @@
    if (is_dir($file)) {
      $f = str_replace(str_replace("%20", " ", $replace), "", $file);
 ?>
-  <tr>
-   <td style='text-align: left'><a onclick="evidenziaFile('<?= $f . $h; ?>')"><i class='fa-solid fa-folder'></i></a> <a href='/u/<?= $pp[0]; ?>/files/<?= $h.$f; ?>/'><b><?= $f; ?>/</b></a></td>
-   <td></td>
-   <td></td>
-   <td><a href='/u/<?= $pp[0]; ?>/deleteDirectory/<?= $h.$f; ?>'><i class='fa fa-trash' aria-hidden='true'></i></a></td>
-   <td></td>
-   <td><a href='/u/<?= $pp[0]; ?>/rename/<?= $h.$f; ?>'><i class='fa-solid fa-pen-to-square'></i></a></td>
-  </tr>
+   <div id='<?= $f . $h; ?>' class='foxcloud-filelist-file w3-display-container'>
+    <a onclick='evidenziaFile("<?= $f . $h; ?>")'><i class="fa fa-folder" aria-hidden="true"></i></a> <a href='/u/<?= $pp[0]; ?>/files/<?= $h.$f; ?>/'><b><?= $f; ?>/</b></a>
+    <a onclick='showSettingsFor(this, "folder");' class='foxcloud-filelist-fileSettings w3-right'><i class="fa fa-bars" aria-hidden="true"></i></a>
+   </div>
 <?php
 
      // echo "<a onclick=\"evidenziaFile($count)\"><i class='fa-solid fa-folder'></i></a> <a href='/u/$pp[0]/files/$h$f/'><b>$f/</b></a><a href='/u/$pp[0]/deleteDirectory/$h$f' style='position: absolute; text-align: right; right: -80%'><i class='fa fa-trash' aria-hidden='true'></i></a><a style='position: absolute; text-align: right; right: -90%' href='/u/$pp[0]/rename/$h$f'><i class='fa-solid fa-pen-to-square'></i></a><br>";
@@ -48,37 +49,85 @@
        $icona = '<i class="fa-solid fa-file"></i>';
      }
 ?>
-  <tr id='<?= $f . $h; ?>'>
-   <td style='text-align: left'><a onclick="evidenziaFile('<?= $h . $f; ?>')"><?= $icona; ?></a> <u><a href='/u/<?= $pp[0]; ?>/fileopen/<?= $h.$f; ?>'><?= $f; ?></a></u></td>
-   <td><?= round(filesize($file)/1000, 2); ?> KB</td>
-   <td><a href='/download?url=<?= $h.$f; ?>'><i class='fa fa-cloud-download' aria-hidden='true'></i></a></td>
-   <td><a href='/u/<?= $pp[0]; ?>/deleteFile/<?= $h.$f; ?>'<i class='fa fa-trash' aria-hidden='true'></i></a></td>
-   <td><a href='/u/<?= $pp[0]; ?>/share/<?= $h.$f; ?>'><i class='fa-solid fa-share-from-square'></i></a></td>
-   <td><a href='/u/<?= $pp[0]; ?>/rename/<?= $h.$f; ?>'><i class='fa-solid fa-pen-to-square'></i></td>
-  </tr>
+   <div id='<?= $f . $h; ?>' class='foxcloud-filelist-file w3-display-container'>
+    <a onclick='evidenziaFile("<?= $f . $h; ?>")'><?= $icona; ?></a> <a href='/u/<?= $pp[0]; ?>/fileopen/<?= $h.$f; ?>'><?= $f; ?></a>
+    <a onclick='showSettingsFor(this, "file");' class='foxcloud-filelist-fileSettings w3-right'><i class="fa fa-bars" aria-hidden="true"></i></a>
+   </div>
 <?php
      // echo "<a onclick=\"evidenziaFile($count)\">$icona</a> <u><a href='/u/$pp[0]/fileopen/$h$f'>$f</a> <span style='position: absolute; text-align: right; right: -50%'>". filesize($file)/1000 . "</span><a href='/download?url=$h$f' style='position: absolute; text-align: right; right: -75%'><i class='fa fa-cloud-download' aria-hidden='true'></i></a> <a style='position: absolute; text-align: right; right: -80%' href='/u/$pp[0]/deleteFile/$h$f'><i class='fa fa-trash' aria-hidden='true'></i></a><a style='position: absolute; text-align: right; right: -85%' href='/u/$pp[0]/share/$h$f'><i class='fa-solid fa-share-from-square'></i></i></a><a style='position: absolute; text-align: right; right: -90%' href='/u/$pp[0]/rename/$h$f'><i class='fa-solid fa-pen-to-square'></i></a></u><br>";
    }
    $count++;
  }
  ?>
-  <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
- </table>
- <br><br><br><br><br><br><br><br><br><br>
+ <br><br>
+ <div id='hudden_fileMenu' class='foxcloud-popup'> 
+  <a class='foxcloud-popupbutton'><i class='fa fa-cloud-download' aria-hidden='true'></i> Scarica</a><br>
+  <a class='foxcloud-popupbutton'><i class='fa-solid fa-share-from-square'></i> Condividi</a><br>
+  <a class='foxcloud-popupbutton'><i class='fa-solid fa-pen-to-square'></i> Rinomina</a><br>
+  <a class='foxcloud-popupbutton'><i class='fa fa-trash' aria-hidden='true'></i>Elimina</a>
+ </div>
+ <div id='hudden_folderMenu' class='foxcloud-popup'>
+  <a class='foxcloud-popupbuttonFolder'><i class='fa-solid fa-pen-to-square'></i> Rinomina</a><br>
+  <a class='foxcloud-popupbuttonFolder'><i class='fa fa-trash' aria-hidden='true'></i> Elimina</a>
+ </div>
+
  <script src='/http.js'></script>
  <script>
+ let showed = false;
+ let last = null;
+
+ const folder = document.getElementById('hudden_folderMenu');
+ const file = document.getElementById('hudden_fileMenu');
+
+ function showSettingsFor(el, type) {
+   if (showed) {
+     folder.style.display = "none";
+     file.style.display = "none";
+     showed = false;
+   }
+ 
+   if (last == el) {
+     folder.style.display = "none";
+     file.style.display = "none";
+     last = null;
+     showed = false;
+     return;
+   }
+
+   let rect = el.getBoundingClientRect();
+   var name = el.parentElement.id;
+   showed = true;
+   last = el;
+   if (type == "folder") {
+     var load = document.getElementsByClassName('foxcloud-popupbuttonFolder');
+     folder.style.top = rect.bottom + document.documentElement.scrollTop + 'px';
+     folder.style.left = rect.left + 'px';
+     folder.style.display = "block";
+     load[0].href = '/u/<?= $pp[0]; ?>/rename/' + name;
+     load[1].href = '/u/<?= $pp[0]; ?>/deleteDirectory/' + name;
+   } else {
+     var load = document.getElementsByClassName('foxcloud-popupbutton');
+     file.style.top = rect.bottom + document.documentElement.scrollTop + 'px';
+     file.style.left = rect.left + 'px';
+     load[0].href = '/download?url=' + name;
+     load[1].href = '/u/<?= $pp[0]; ?>/share/' + name;
+     load[2].href = '/u/<?= $pp[0]; ?>/rename/' + name;
+     if (name == '' || name == '/') {
+       alert("INVALID INPUT!");
+       return;
+     }
+     load[3].href = '/u/<?= $pp[0]; ?>/deleteFile/' + name;
+     file.style.display = "block";
+   }
+ }
+
  function evidenziaFile(number) {
     var res = JSON.parse(http_request('/evidenziaFile?file=' + number));
     alert(res.message);
     location.reload(); 
  }
 
- document.getElementById('body').addEventListener('load', loadPage());
-
- function loadPage() {
-   loadTableWidth();
-   loadFileEvidenziati();
- }
+ document.getElementById('body').addEventListener('load', loadFileEvidenziati());
 
  function loadFileEvidenziati() {
    var res = JSON.parse(http_request('/getEvidenziati'));
@@ -93,8 +142,4 @@
      }
    }
  }
-
- function loadTableWidth() {
-   document.getElementById('mytable').style.width = (screen.width - 50) + "px";
- } 
  </script>
