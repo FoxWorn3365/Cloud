e15494f42846922465f62288430b03c45a84bb46
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 21:48:34 +0100

    Rimossa la table ed implementata una classe per lo stile

--- protected/components/files_header.php
+++ protected/components/files_header.php
@@ -4,16 +4,9 @@ for ($a = 0; $a < count($textAbout)-2; $a++) {
   $list = $list . '/' . $textAbout[$a];
 }
 ?>
- <br>
- <a href="/u/<?= $pp[0]; ?>/uploadFile/<?= str_replace("*", "", $bb); ?>"><i class="fa fa-cloud-upload" aria-hidden="true"></i></i></a> | <a href="/u/<?= $pp[0]; ?>/createDir/<?= str_replace("*", "", $bb); ?>"><i class="fa fa-folder" aria-hidden="true"></i></a> | <a href="/u/<?= $pp[0]; ?>/new/<?= str_replace("*", "", $bb); ?>"><i class="fa-solid fa-circle-plus"></i></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Directory: <b>/<?= $bb; ?></b><?php if ($dd != "*") { ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/u/<?= $pp[0]; ?>/files<?= $list; ?>/"><i class="fa fa-level-up" aria-hidden="true"></i></a> <?php } ?>
- <hr>
- <br>
- <table style='width: 100%; border: none; position: absolute; left: 25px;' id='mytable'>
-  <tr>
-   <th><!-- filename --></th>
-   <th><!-- peso --></th>
-   <th><!-- scarica --></th>
-   <th><!-- elimina --></th>
-   <th><!-- condividi --></th>
-   <th><!-- rinomina --></th>
-  </tr>
+ <div id='fileList' style='text-align: left'>
+  <div id='fileList_menu' class='foxcloud-filemenu'>
+   <a href="/u/<?= $pp[0]; ?>/uploadFile/<?= str_replace("*", "", $bb); ?>"><i class="fa fa-cloud-upload" aria-hidden="true"></i></i></a> | <a href="/u/<?= $pp[0]; ?>/createDir/<?= str_replace("*", "", $bb); ?>"><i class="fa fa-folder" aria-hidden="true"></i></a> | <a href="/u/<?= $pp[0]; ?>/new/<?= str_replace("*", "", $bb); ?>"><i class="fa-solid fa-circle-plus"></i></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Directory: <b>/<?= $bb; ?></b><?php if ($dd != "*") { ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/u/<?= $pp[0]; ?>/files<?= $list; ?>/"><i class="fa fa-level-up" aria-hidden="true"></i></a> <?php } ?>
+  </div>
+  <br>
+  <div id='fileList_list' class='foxcloud-filelist'>
