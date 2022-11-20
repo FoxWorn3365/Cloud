8ab0fac1b8314c62a6d8cfde3548b2d3c502f964
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:15:53 +0100

    Aggiornate le frasi per rendere più adeguate a tutti

--- protected/components/uploadfile_header.php
+++ protected/components/uploadfile_header.php
@@ -1,7 +1,24 @@
+<?php
+// +-----------------------------------+
+// |             FoxCloud              |
+// +-----------------------------------+
+// | Questo file fa parte del progetto |
+// | di Cloud Open Source "FoxCloud",  |
+// | realizzato da FoxWorn.            |
+// +-----------------------------------+
+// | Web: https://foxcloud.fcosma.it   |
+// | GH: github.com/FoxWorn3365/Cloud  |
+// | License: GNU GPL 3.0              |
+// +-----------------------------------+
+// | You can write me an email at:     |
+// | foxworn3365@gmail.com, also for   |
+// | talk!                             |
+// +-----------------------------------+
+?>
 <br>
  <h1>Carica un file sul Cloud</h1>
  <a href='<?= $_SERVER["HTTP_REFERER"]; ?>'><i class="fa fa-level-up" aria-hidden="true"></i></a><br>
  <hr>
  <br>
- <b>Ricordiamo che la velocità in upload per ogni utente è di 3,5Mb/s quindi per i file pesanti ci potrebbe volere un po.<br>La massima dimensione è di <u>3GB</u> per file</b>
+ <b>Per caricare un file trascinalo sull'apposito form.<br>La massima dimensione dipende dal vostro WebMaster.</b>
  <br><br>
