006b44a9e8a8bfdbad19ed43c5af2180411eab1e
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:04:13 +0100

    Applicato il marchio

--- protected/components/build_admin_dashboard.php
+++ protected/components/build_admin_dashboard.php
@@ -1,4 +1,20 @@
 <?php
+// +-----------------------------------+
+// |             FoxCloud              |
+// +-----------------------------------+
+// | Questo file fa parte del progetto |
+// | di Cloud Open Source "FoxCloud",  |
+// | realizzato da FoxWorn.            |
+// +-----------------------------------+
+// | Web: https://foxcloud.fcosma.it   |
+// | GH: github.com/FoxWorn3365/Cloud  |
+// | License: GNU GPL 3.0              |
+// +-----------------------------------+
+// | You can write me an email at:     |
+// | foxworn3365@gmail.com, also for   |
+// | talk!                             |
+// +-----------------------------------+
+
 require_once("protected/components/header.php");
 
 // Peso di una dir
