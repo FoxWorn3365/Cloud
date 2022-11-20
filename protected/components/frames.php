47437e859d74d62265709de1c00c0078fbc31ea3
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:09:46 +0100

    Update frames.php

--- protected/components/frames.php
+++ protected/components/frames.php
@@ -1,3 +1,20 @@
+<?php
+// +-----------------------------------+
+// |             FoxCloud              |
+// +-----------------------------------+
+// | Questo file fa parte del progetto |
+// | di Cloud Open Source "FoxCloud",  |
+// | realizzato da FoxWorn.            |
+// +-----------------------------------+
+// | Web: https://foxcloud.fcosma.it   |
+// | GH: github.com/FoxWorn3365/Cloud  |
+// | License: GNU GPL 3.0              |
+// +-----------------------------------+
+// | You can write me an email at:     |
+// | foxworn3365@gmail.com, also for   |
+// | talk!                             |
+// +-----------------------------------+
+?>
   <div id='userinfo' class='foxcloud-userinfo' style='display: none'>
 <?php
 $info = json_decode(file_get_contents('protected/users/' . $_SESSION["user"] . '/userinfo.conf'));
