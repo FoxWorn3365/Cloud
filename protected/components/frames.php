a2bac29cfc937e663c488fab9c11bcfe9725eea5
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 23:20:40 +0100

    "Aggiunte" le impostazioni

--- protected/components/frames.php
+++ protected/components/frames.php
@@ -32,9 +32,6 @@ $info = json_decode(file_get_contents('protected/users/' . $_SESSION["user"] . '
    <b><?= $gb; ?></b> <progress max='<?= $info->diskSpace; ?>' value='<?= $gb; ?>'></progress> <b><?= $info->diskSpace; ?></b><br><br>
   </div>
   <script>
-  document.body.onload = function() {
-    document.getElementById('userinfo').style.top = document.getElementById('navbar').offsetHeight + 'px';
-  }
   var b = 0;
   let a = document.getElementById('userinfo');
   function showUserInfo() {
@@ -47,3 +44,28 @@ $info = json_decode(file_get_contents('protected/users/' . $_SESSION["user"] . '
     }
   }
   </script>
+  <!-- SEPARATORIO > IMPOSTAZIONI -->
+  <div id='settings' class='foxcloud-settings'>
+   <a href='/logout' class='foxcloud-button' style='float: right'><i class="fa fa-sign-out" aria-hidden="true"></i></a>
+   <h1>Hej!</h1>
+   Al momento non ci sono impostazioni da impostare, provvederemo ad aggiungerle nella v1.9.<br>
+  </div>
+  <script>
+  document.body.onload = function() {
+    document.getElementById('userinfo').style.top = document.getElementById('navbar').offsetHeight + 'px';
+    document.getElementById('settings').style.top = document.getElementById('navbar').offsetHeight + 'px';
+  }
+
+  var d = 0;
+  let c = document.getElementById('settings');
+  function showUserSettings() {
+    if (d == 0) {
+      c.style.display = "block";
+      d = 1;
+    } else {
+      c.style.display = "none";
+      d = 0;
+    }
+  }
+  </script>
+
