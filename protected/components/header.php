0e2ccf935c307a60f4e1d5a2cc4e52e3d88b7a8c
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:43:51 +0100

    Rimossi i footer e la lightMode
    Finalmente quei footers super buggati sono spariti ed anche la maledetta lightMode, ora infatti si potrà navigare nel cloud SOLO con la bene amata darkMode

--- protected/components/header.php
+++ protected/components/header.php
@@ -38,7 +38,7 @@ if (empty($_SESSION["user"])) {
   <link rel="stylesheet" href="/local.css">
   <link rel="stylesheet" href="https://resources.fcosma.it/fa/css/all.min.css">
  </head>
- <body style="text-align: center; background: white" id='body'>
+ <body style="text-align: center; background: black; color: white;height: fit-content" id='body'>
   <div id='temp_1' style='display: none'></div>
   <div class="w3-bar foxcloud-navbar" id='navbar'>
 <?php
@@ -65,23 +65,8 @@ if (isset($b)) {
   require 'protected/components/frames.php';
 }
 ?>
-  <div id='footer' class='w3-container'  style='padding: 5px; position: absolute; left: 0px; width: 100%;'>
-   <br><br>
-   <div id='footerContent' class='w3-white' style='padding: 0px; position: absolute; bottom: 0px; text-align: center; width: 100%'>
-    <span>&copy; 2021 - 2022 <a href='https://foxcloud.fcosma.it/'>FoxCloud</a> by <a href='https://github.com/FoxWorn3365'>FoxWorn3365</a> | Rilasciato sotto <a href='https://github.com/FoxWorn3365/Cloud/blob/v1.5/LICENSE'>GNU General Public License v3.0</a><a class='w3-right w3-button' onclick='changeTon()'><i id='ton' class="fa-solid fa-sun"></i></a></span>
-   </div>
-  </div>
   <script>
-  let body = document.body;
-  let html = document.documentElement;
-
-  const height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-
-  document.body.onload = function() {
-     document.getElementById('footer').style.display = "block";
-     document.getElementById('footer').style.top = height - 50 + "px";
-  }
-
+/*   RICORDI
   function isInDarkMode() {
     if (document.cookie.split(';').some((item) => item.includes('mode=dark'))) {
       return true;
@@ -89,57 +74,8 @@ if (isset($b)) {
       return false;
     }
   }
+*/
 
-  // caricamento
-  document.getElementById('body').addEventListener('load', checkView());
-  
-  (function() {
-    if (!isInDarkMode()) {
-      changeTon();
-    }
-  }());
-
-  function checkView() {
-    if (isInDarkMode()) {
-      document.getElementById('ton').classList = "fa-solid fa-sun";
-      changeTon();
-    } else {
-      document.getElementById('ton').classList = "fa-solid fa-moon";
-      changeTon();
-    }
-  }
-
-  function setViewCookie(mode) {
-    if (mode == "dark") {
-      document.cookie = "mode=dark; Secure";
-    } else {
-      document.cookie = "mode=light; Secure";
-    }
-  }
-      
-  function changeTon() {
-    document.getElementById('temp_1').innerHTML = Number(document.getElementById('temp_1').innerHTML) + 1;
- 
-    if (Number(document.getElementById('temp_1').innerHTML) == 5) {
-      window.location.href = "/admin/easter-egg/bazinga";
-    }
-
-    if (document.getElementById('ton').classList == "fa-solid fa-sun") {
-      // Mettiamo in modalità notte
-      document.getElementById('ton').classList = "fa-solid fa-moon";
-      document.getElementById('body').style.color = "white";
-      document.getElementById('body').style.backgroundColor = "black";
-      document.getElementById('footerContent').classList = "w3-black w3-text-white";
-      setViewCookie('dark');
-    } else {
-      document.getElementById('ton').classList = "fa-solid fa-sun";
-      document.getElementById('body').style.color = "black";
-      document.getElementById('body').style.backgroundColor = "white";
-      document.getElementById('footerContent').classList = "w3-white w3-text-black";
-      setViewCookie('light');
-    }
-  }
-   
   function getDevice() {
     const ua = navigator.userAgent;
     if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
