88137d415ed244bf7b6f1ac78c6e9294ec61829d
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:06:17 +0100

    Update build_files.php

--- protected/components/build_files.php
+++ protected/components/build_files.php
@@ -1,4 +1,21 @@
- <br>
+<?php
+// +-----------------------------------+
+// |             FoxCloud              |
+// +-----------------------------------+
+// | Questo file fa parte del progetto |
+// | di Cloud Open Source "FoxCloud",  |
+// | realizzato da FoxWorn.            |
+// +-----------------------------------+
+// | Web: https://foxcloud.fcosma.it   |
+// | GH: github.com/FoxWorn3365/Cloud  |
+// | License: GNU GPL 3.0              |
+// +-----------------------------------+
+// | You can write me an email at:     |
+// | foxworn3365@gmail.com, also for   |
+// | talk!                             |
+// +-----------------------------------+
+?>
+<br>
  <style>
  a {
   text-decoration: none;
