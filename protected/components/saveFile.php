1ce85994d47d9152dcb7331c4a034a1df4da6476
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:13:37 +0100

    Update saveFile.php

--- protected/components/saveFile.php
+++ protected/components/saveFile.php
@@ -1,4 +1,20 @@
 <?php
+// +-----------------------------------+
+// |             FoxCloud              |
+// +-----------------------------------+
+// | Questo file fa parte del progetto |
+// | di Cloud Open Source "FoxCloud",  |
+// | realizzato da FoxWorn.            |
+// +-----------------------------------+
+// | Web: https://foxcloud.fcosma.it   |
+// | GH: github.com/FoxWorn3365/Cloud  |
+// | License: GNU GPL 3.0              |
+// +-----------------------------------+
+// | You can write me an email at:     |
+// | foxworn3365@gmail.com, also for   |
+// | talk!                             |
+// +-----------------------------------+
+
 // v1.5
 $contents = filter_var($_POST["file"], FILTER_SANITIZE_STRING);
 $bb = str_replace("%20", " ", $bb);
