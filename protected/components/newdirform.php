4a564b6f8b2697228d68e8a3cf2ace29d61b41a6
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:26:53 +0100

    Aggiornato seguendo l'ultimo stile

--- protected/components/newdirform.php
+++ protected/components/newdirform.php
@@ -26,9 +26,10 @@ $bb = str_replace("%20", " ", $bb);
  <form method="post" action="/newdir">
    <input type="hidden" name="user" value="<?= $pp[0]; ?>">
    <input type="hidden" name="dir" value="<?= $bb; ?>">
-   Inserisci il nome: <input type="text" name="nomedelladirectory" id='dirname' maxlength='25'><br>
-   <br><button class="w3-button w3-green w3-text-white">Crea</button>
+   Inserisci il nome: <input type="text" name="nomedelladirectory" id='dirname' maxlength='25' class='foxcloud-input'><br>
+   <br><button class="foxcloud-button">Crea</button>
  </form>
  <script>
  document.getElementById('dirname').addEventListener('input', () => { document.getElementById('newdirname').innerHTML = document.getElementById('dirname').value; });
  </script>
+
