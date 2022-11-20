4c35876e901044475a69f0180b0cbf297a4c5f4c
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:45:08 +0100

    Aggiornato l'editor in modo stiloso

--- protected/components/build_visualEditor.php
+++ protected/components/build_visualEditor.php
@@ -19,5 +19,6 @@ require_once("protected/components/editfile_header.php");
 
 $bb = str_replace("%20", " ", $bb);
 ?>
-  <textarea name="contenuto" style="width: 80%; height: 500px"><?= file_get_contents("protected/disk/$user->dir/$bb"); ?></textarea>
+  <textarea name="contenuto" style="width: 80%; height: 500px" class='foxcloud-input'><?= file_get_contents("protected/disk/$user->dir/$bb"); ?></textarea>
 </form>
+
