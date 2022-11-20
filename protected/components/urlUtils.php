3ba77547a3157ca8b370a7ee2ea3f5a636bf83bc
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 23:04:43 +0100

    Non risulta essere fondamentale per FoxCloud

--- protected/components/urlUtils.php
+++
@@ -1,6 +0,0 @@
-<?php
-function urlEncode($string) {
-    $entities = array('%21', '%2A', '%27', '%28', '%29', '%3B', '%3A', '%40', '%26', '%3D', '%2B', '%24', '%2C', '%2F', '%3F', '%25', '%23', '%5B', '%5D');
-    $replacements = array('!', '*', "'", "(", ")", ";", ":", "@", "&", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]");
-    return str_replace($entities, $replacements, urlencode($string));
-}
