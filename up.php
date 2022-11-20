41f83e098e05702137fefbbd49cef775c6733021
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:23:19 +0100

    Update up.php

--- up.php
+++ up.php
@@ -1,4 +1,20 @@
 <?php
+// +-----------------------------------+
+// |             FoxCloud              |
+// +-----------------------------------+
+// | Questo file fa parte del progetto |
+// | di Cloud Open Source "FoxCloud",  |
+// | realizzato da FoxWorn.            |
+// +-----------------------------------+
+// | Web: https://foxcloud.fcosma.it   |
+// | GH: github.com/FoxWorn3365/Cloud  |
+// | License: GNU GPL 3.0              |
+// +-----------------------------------+
+// | You can write me an email at:     |
+// | foxworn3365@gmail.com, also for   |
+// | talk!                             |
+// +-----------------------------------+
+
 // Valori banditi
 $special_chars = array("?", "[", "]", "/", "\\", "=", "<", ">", ":", ";", ",", "'", "\"", "&", "$", "#", "*", "(", ")", "|", "~", "`", "!", "{", "}", "%", "+", chr(0));
 
