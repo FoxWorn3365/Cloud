23caad9e857b0690f832acb0afeb7bb6ee1f10bb
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-20 22:17:29 +0100

    Update audio.php

--- audio.php
+++ audio.php
@@ -1,4 +1,20 @@
 <?php
+// +-----------------------------------+
+// |             FoxCloud              |
+// +-----------------------------------+
+// | Questo file fa parte del progetto |
+// | di Cloud Open Source "FoxCloud",  |
+// | realizzato da FoxWorn.            |
+// +-----------------------------------+
+// | Web: https://foxcloud.fcosma.it   |
+// | GH: github.com/FoxWorn3365/Cloud  |
+// | License: GNU GPL 3.0              |
+// +-----------------------------------+
+// | You can write me an email at:     |
+// | foxworn3365@gmail.com, also for   |
+// | talk!                             |
+// +-----------------------------------+
+
 $u = filter_var($_GET["user"], FILTER_SANITIZE_STRING);
 $type = filter_var($_GET["type"], FILTER_SANITIZE_STRING);
 $shared = filter_var($_GET["sharedurl"], FILTER_SANITIZE_STRING);
