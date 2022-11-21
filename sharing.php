87748d7a03d69178cf2a96db11444b97ede50b8b
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-21 10:29:56 +0100

    Aggiornato lo stile e il redirect seguendo la dash

--- sharing.php
+++ sharing.php
@@ -60,8 +60,7 @@ require("protected/components/header.php");
  <b>Link dello shared:</b> <a onclick='copyurl()'>https://cloud.fcosma.it/s/<?= $s; ?></a><br>
  <b>Password:</b> <?= $password; ?>
  <br><br><br>
- <a href="/u/<?= $user; ?>/sharedList/"><button class="w3-button w3-orange w3-text-white">Vai agli Shared</button></a>
- <a href="/u/<?= $user; ?>/file"><button class="w3-button w3-orange w3-text-white">Vai ai tuoi File</button></a>
+ <a href="/u/<?= $user; ?>/files/"><button class="foxcloud-button">Torna ai tuoi File</button></a>
  <br><br><br>
  <script>
  function copyurl() {
