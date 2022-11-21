bc93c7256e8ea6671ac733802fb29fd1cebb9b11
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-21 18:27:01 +0100

    Aggiornato seguendo il nuovo stile
    altra piccolissima dimenticanza

--- protected/components/wiew_header.php
+++ protected/components/wiew_header.php
@@ -26,7 +26,8 @@ if (!empty($link)) {
 }
 ?>
 <br>
-  <?php if ($ext[$cc-1] == "txt" || $ext[$cc-1] == "md" || $ext[$cc-1] == "text" || $ext[$cc-1] == "wrf") {?>  <a href="/u/<?= $pp[0]; ?>/editfile/<?= $bb; ?>"><i class="fa fa-pencil-square" aria-hidden="true"></i></a> | <?php } ?><a href="/u/<?= $pp[0]; ?>/deleteFile/<?= $bb; ?>"><i class="fa fa-trash" aria-hidden="true"></i></a> | <a href='/u/<?= $pp[0]; ?>/share/<?= $bb; ?>'><i class="fa-solid fa-share-from-square"></i></a> | <a href="<?= $download; ?>"><i class="fa fa-cloud-download" aria-hidden="true"></i></a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; File: <b>/<?= $bb; ?></b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/u/<?= $pp[0]; ?>/files/<?= $list; ?>"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i></a>
-  <br>
-  <hr>
+ <div id='fileList' style='text-align: center'>
+  <div id='fileList_menu' class='foxcloud-filemenu' style='text-align: left'>
+   <?php if ($ext[$cc-1] == "txt" || $ext[$cc-1] == "md" || $ext[$cc-1] == "text" || $ext[$cc-1] == "wrf") {?>  <a href="/u/<?= $pp[0]; ?>/editfile/<?= $bb; ?>"><i class="fa fa-pencil-square" aria-hidden="true"></i></a> | <?php } ?><a href="/u/<?= $pp[0]; ?>/deleteFile/<?= $bb; ?>"><i class="fa fa-trash" aria-hidden="true"></i></a> | <a href='/u/<?= $pp[0]; ?>/share/<?= $bb; ?>'><i class="fa-solid fa-share-from-square"></i></a> | <a href="<?= $download; ?>"><i class="fa fa-cloud-download" aria-hidden="true"></i></a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; File: <b>/<?= $bb; ?></b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/u/<?= $pp[0]; ?>/files/<?= $list; ?>"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i></a>
+  </div>
   <br>
