1b0c0c682b00c85452c253509a79d282f6e37eed
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-23 22:07:45 +0100

    Aggiornato con le impostazioni

--- protected/components/files_header.php
+++ protected/components/files_header.php
@@ -19,10 +19,12 @@ $textAbout = explode('/', $bb);
 for ($a = 0; $a < count($textAbout)-2; $a++) {
   $list = $list . '/' . $textAbout[$a];
 }
+
+$localS = json_decode(file_get_contents('protected/sys/.' . $_SESSION["user"] . '_preferences.sys'));
 ?>
  <div id='fileList' style='text-align: left'>
   <div id='fileList_menu' class='foxcloud-filemenu w3-display-container'>
-   <a href="/u/<?= $pp[0]; ?>/uploadFile/<?= str_replace("*", "", $bb); ?>"><i class="fa fa-cloud-upload" aria-hidden="true"></i></i></a> | <a href="/u/<?= $pp[0]; ?>/createDir/<?= str_replace("*", "", $bb); ?>"><i class="fa fa-folder" aria-hidden="true"></i></a> | <a href="/u/<?= $pp[0]; ?>/new/<?= str_replace("*", "", $bb); ?>"><i class="fa-solid fa-circle-plus"></i></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Directory: <b>/<?= $bb; ?></b><?php if ($dd != "*") { ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/u/<?= $pp[0]; ?>/files<?= $list; ?>/"><i class="fa fa-level-up" aria-hidden="true"></i></a> <?php } ?> <input type='text' class='foxcloud-fileheader-input w3-display-middle' placeholder='banana.txt' id='fileSearchTerm'>
+   <a href="/u/<?= $pp[0]; ?>/uploadFile/<?= str_replace("*", "", $bb); ?>"><i class="fa fa-cloud-upload" aria-hidden="true"></i></i></a> | <a href="/u/<?= $pp[0]; ?>/createDir/<?= str_replace("*", "", $bb); ?>"><i class="fa fa-folder" aria-hidden="true"></i></a> | <a href="/u/<?= $pp[0]; ?>/new/<?= str_replace("*", "", $bb); ?>"><i class="fa-solid fa-circle-plus"></i></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Directory: <b>/<?= $bb; ?></b><?php if ($dd != "*") { ?> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/u/<?= $pp[0]; ?>/files<?= $list; ?>/"><i class="fa fa-level-up" aria-hidden="true"></i></a> <?php } ?> <?php if ($localS->searchBar == "true") { ?><input type='text' class='foxcloud-fileheader-input w3-right' placeholder='banana.txt' id='fileSearchTerm'> <?php } ?>
   </div>
   <br>
   <div id='fileList_list' class='foxcloud-filelist'>
