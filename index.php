f15cd40c999dc101aefdce11d8ca3f1cac2dfc79
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-24 16:09:43 +0100

    Aggiornata aggiungendo contenuti neutri

--- index.php
+++ index.php
@@ -19,4 +19,28 @@ session_start();
 require_once("protected/components/header.php");
 ?>
 <br>
-<h1>Fcosma.it - Cloud</h1>
+ <div class='foxcloud-homepage-box w3-display-container' style='width: 50%'>
+  <span style='font-size: 50px'><b>FoxCloud</b></span>
+  <div class='foxcloud-homepage-inBox-left'>
+   Un nuovo modo di gestire i tuoi file online.
+  </div>
+ </div>
+ <br>
+ <div class='foxcloud-homepage-box w3-display-container'>
+  <span style='font-size: 30px'><b>Cosa è FoxCloud?</b></span>
+  <div class='foxcloud-homepage-inBox-left'>
+   FoxCloud è un file manager (o comunemente chiamato Cloud) online, realizzato quasi interamente in PHP per la gestione dei tuoi file in rete.<br>
+   Grazie alla sua interfaccia user-friendly, FoxCloud è semplicissimo da usare e questo ti garantisce facilità nell'accedere ai tuoi file online.
+  </div>
+ </div>
+ <br>
+ <div class='foxcloud-homepage-box w3-display-container'>
+  <span style='font-size: 30px'><b>Come nasce FoxCloud?</b></span>
+  <div class='foxcloud-homepage-inBox-left'>
+   FoxCloud nasce all'inizio del 2022, sviluppato inizialmente in PHP puro per la sola gestione di file online con un'interfaccia grafica complessa,<br>
+   datata e che non si adeguava alle richieste odierne che richiedono uno stile sempre più innovativo e complesso.<br><br>
+   Il Cloud fa la sua comparsa su GitHub il 24 Febbraio 2022 dove venne caricata la versione <b>v0.9</b>, ovvero una beta che<br>
+   avrebbe dopo poco tempo lasciato spazio ad un cloud più aperto, funzionale e facile da usare.<br><br>
+   Dal 16 Luglio 2022 FoxCloud supporta l'aggiunta di plugins scritti in PHP e supportati da apposite API, documentate su GitHub.
+  </div>
+ </div>
