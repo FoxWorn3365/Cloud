c54d984bf6627fa503a21c69ca8c3b27561f1e70
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-11-24 16:37:48 +0100

    Aggiunto il file per il check di FoxCloudWorld

---
+++ foxcloudrwapper.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------+
+// |             FoxCloud              |
+// +-----------------------------------+
+// | Questo file fa parte del progetto |
+// | di Cloud Open Source "FoxCloud",  |
+// | realizzato da FoxWorn.            |
+// +-----------------------------------+
+// | Web: https://foxcloud.fcosma.it   |
+// | GH: github.com/FoxWorn3365/Cloud  |
+// | License: GNU GPL 3.0              |
+// +-----------------------------------+
+// | You can write me an email at:     |
+// | foxworn3365@gmail.com, also for   |
+// | talk!                             |
+// +-----------------------------------+
+
+die(json_encode(array('status' => 200, 'isFoxCloudIstance' => true, 'codeCheck' => file_get_contents('protected/sys/foxworldverifycodemain.temp.txt'))));
