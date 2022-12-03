8f50f402d50ca4c9bfbb8127962f177d10fa915f
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-03 23:08:32 +0100

    Aggiunta la funzione per "risparmiare" sugli shared

--- sharing.php
+++ sharing.php
@@ -41,6 +41,33 @@ $file = str_replace("https://$server/u/$user/share/", "", $url);
 $s = randomString(20);
 $string = "$user{}file{}$file";
 
+$cloudConf = json_decode(file_get_contents('protected/config/config.json'));
+
+if ($cloudConf->fewShared) {
+  foreach (glob('protected/shared/*') as $shared) {
+    $sh = file_get_contents($shared);
+    $shared = str_replace('protected/shared/', '', $shared);
+    if ($sh == $string && empty(explode('{}', $sh)[3])) {
+      require_once('protected/components/header.php');
+?>
+ <br>
+  <h1>File condiviso - FoxCloud</h1>
+  Il file non è stato propriamente condiviso, a causa di un'impostazione voluta dall'admin non puoi creare più link di condivisione per un unico file.<br>
+  Pertanto quello che sta per essere fornito sarà quello già esistente. Ti ricordiamo che puoi sempre eliminare i tuoi shared<br><br>
+  <b>Codice dello shared:</b> <?= $shared; ?><br>
+  <b>Link dello shared:</b> <a onclick='copyurl()' title='Clicca per copiare'>https://cloud.fcosma.it/s/<?= $shared; ?></a><br><br><br>
+  <a href="/u/<?= $user; ?>/files/"><button class="foxcloud-button">Torna ai tuoi File</button></a>
+  <script>
+  function copyurl() {
+    navigator.clipboard.writeText('https://cloud.fcosma.it/s/<?= $shared; ?>');
+  }
+  </script>
+<?php
+      die();
+    }
+  }
+}
+
 if (!empty($password)) {
   // Ok, niente password
   file_put_contents("protected/shared/$s", $string . '{}' . $password);
@@ -57,7 +84,7 @@ require("protected/components/header.php");
  <h3 style="color: green">File condiviso con successo!</h3>
  <br><br><br>
  <b>Codice dello shared:</b> <?= $s; ?><br>
- <b>Link dello shared:</b> <a onclick='copyurl()'>https://cloud.fcosma.it/s/<?= $s; ?></a><br>
+ <b>Link dello shared:</b> <a onclick='copyurl()' title='Clicca per copiare'>https://cloud.fcosma.it/s/<?= $s; ?></a><br>
  <b>Password:</b> <?= $password; ?>
  <br><br><br>
  <a href="/u/<?= $user; ?>/files/"><button class="foxcloud-button">Torna ai tuoi File</button></a>
