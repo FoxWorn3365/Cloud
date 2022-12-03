9e7b3e43855e6434546c839e7d6a672497754539
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-03 23:09:27 +0100

    Aggiunto il bottone

--- protected/components/shareAFile.php
+++ protected/components/shareAFile.php
@@ -25,7 +25,7 @@ $bb = str_replace("%20", " ", $bb);
  <form method="post" action="/sharing">
    Inserisci una password.<br><u>Se non desideri usare una password lascia vuoto</u><br>
    <input type="text" name="typePasswordOfShared1" class='foxcloud-input'><br><br>
-   <button class="w3-button w3-orange w3-text-white">Condividi</button>
+   <button class="foxcloud-button">Condividi</button>
  </form>
  <br><br><br>
  <span style="color: red"><b>ATTENZIONE!</b> Se non imposti una password tutti coloro che hanno il link potranno accedere al contenuto!</span>
