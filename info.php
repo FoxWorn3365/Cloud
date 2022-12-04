6628c9c821c1d96db3e6ee2233e861024f8a562b
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-12-04 19:13:46 +0100

    Aggiornato seguendo i nuovi standard di FCW

--- info.php
+++ info.php
@@ -65,35 +65,3 @@ if (file_exists('protected/sys/foxworldverifycodemain.temp.txt')) {
   </div>
  </div>
  <br><br><br><br>
-  <span style='font-size: 30px'><b>Pacchetti usati</b></span>
-  <div class='foxcloud-homepage-inBox-left'>
-   <a href='https://github.com/FoxWorn3365/FoxPlayer'><b>FoxPlayer</b></a> <i>Player di video. Versione adattata a FoxCloud.</i><br>
-   <a href='https://github.com/erusev/parsedown'><b>Parsedown</b></a> <i>Gestore del markdown per i file .md</i>
-  </div>
- </div>
- <br>
- <div class='foxcloud-homepage-box w3-display-container'>
-  <span style='font-size: 30px'><b><i class="fa-solid fa-earth-europe"></i> FoxCloud World</b></span>
-  <div class='foxcloud-homepage-inBox-left'>
-<?php
-// FoxCloud World Loader
-if (file_exists('protected/sys/foxworld.key')) {
-  $key = file_get_contents('protected/sys/foxworld.key');
-
-  $is = json_decode(file_get_contents('https://foxcloud.fcosma.it/api/world/get?host=' . $key));
-  if ($is->status == 200) {
-?>
-   Questa istanza di <b>FoxCloud</b> risulta al momento correttamente registrata tramite <u>FoxCloudWorld</u>.<br>
-   <h3>Istanza: foxcloudworld-<?= $is->data->id; ?></h3>
-   Presente sui nostri sistemi dal <code><?= date('d/m/Y - H:i:s', $is->data->date); ?></code> - Istanza N°<?= $is->data->number; ?> di FoxCloud registrata.
-<?php
-  }
-} else {
-?>
-   Istanza non presente su <b>FoxCloudWorld</b>, probabilmente l'opzione è stata disattivata dalla configurazione!
-<?php
-}
-?>
-  </div>
- </div>
- <br><br><br><br>
